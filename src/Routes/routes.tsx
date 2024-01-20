dd7ab96083b6b8234962c674d1077e9fc9835eaa
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-01-20 19:29:32 -0300

    create routes

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,7 +1,16 @@
-export default function Routes() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../Pages/Login/login";
+import Cadastro from "../Pages/Cadastro/cadastro";
+
+export default function RoutesApp() {
     return(
         <>
-            
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login/>}/>
+                    <Route path="cadastro" element={<Cadastro/>}/>
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
