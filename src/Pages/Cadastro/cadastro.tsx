d2f53f712bae5e57b6dd8efa3baced0c92694fdf
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-01-21 10:17:36 -0300

    formCadastro for page cadastro

--- src/Pages/Cadastro/cadastro.tsx
+++ src/Pages/Cadastro/cadastro.tsx
@@ -1,9 +1,13 @@
+import FormCadastro from "../../components/Form-cadastro/form-cadastro";
 import Header from "../../components/Header/header";
 
 export default function Cadastro() {
-    return(
+    return (
         <>
-            <Header/>
+            <Header />
+            <div className="flex justify-center items-center mt-8 sm:flex sm:justify-center sm:items-center">
+                <FormCadastro />
+            </div>
         </>
     )
 }
