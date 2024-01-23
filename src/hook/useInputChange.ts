2c781f1cb9ee3b1e11c9aac991fb548dc4685259
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-01-23 12:57:51 -0300

    hook input change value

---
+++ src/hook/useInputChange.ts
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+export default function useInputChange() {
+
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [nome, setNome] = useState('')
+    const [telefoneData, setTelefoneData] = useState({
+        ddd: "",
+        Telefone: ""
+    })
+    const [genero, setGenero] = useState({
+        masculino: "",
+        feminino: "",
+        outro: ""
+    })
+
+    function handleEmailValue(e: any) {
+        setEmail(e.target.value)
+    }
+
+    function handleSenhaValue(e: any) {
+        setSenha(e.target.value)
+    }
+
+    function handleTelefoneValue(e: any) {
+        setTelefoneData({ ...telefoneData, [e.target.name]: e.target.value })
+    }
+
+    function handleNomeInput(e: any) {
+        setNome(e.target.value)
+    }
+
+    function handleGeneroValue(e: any) {
+        setGenero(e.target.value)
+    }
+
+    return {
+        email,
+        senha,
+        nome,
+        telefoneData,
+        genero,
+        handleEmailValue,
+        handleSenhaValue,
+        handleTelefoneValue,
+        handleNomeInput,
+        handleGeneroValue
+    }
+}
