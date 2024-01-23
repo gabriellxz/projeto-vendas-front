496098ac5c9dae132ddac7da9f5e4976088b1f9b
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-01-23 12:57:08 -0300

    types input props

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -2,13 +2,19 @@ interface Props {
     typeInput: string;
     inputLabel: string;
     styleWidth: string;
+    name: string;
+    value: string;
+    onInputValue: (e:any) => void;
 }
 
 export default function Input(props: Props) {
+
+    
+
     return (
         <>
             <label className="text-xl">{props.inputLabel}</label>
-            <input type={props.typeInput} className={`
+            <input type={props.typeInput} value={props.value} onChange={props.onInputValue} name={props.name} className={`
                 border border-1 border-black outline-none p-2 ${props.styleWidth}
             `}/>
         </>
