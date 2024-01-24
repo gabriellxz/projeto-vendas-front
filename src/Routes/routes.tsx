7821afb9cbcdd92626cb5a6030f8a66d7f178795
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-01-24 17:12:49 -0300

    add new routes

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../Pages/Login/login";
 import Cadastro from "../Pages/Cadastro/cadastro";
+import Home from "../Pages/Home/home";
 
 export default function RoutesApp() {
     return(
@@ -9,6 +10,7 @@ export default function RoutesApp() {
                 <Routes>
                     <Route path="/" element={<Login/>}/>
                     <Route path="cadastro" element={<Cadastro/>}/>
+                    <Route path="home" element={<Home/>}/>
                 </Routes>
             </BrowserRouter>
         </>
