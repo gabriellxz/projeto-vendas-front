71d85bd4324158ea6ea9413d1b6b64d1cd4298fa
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-01-27 22:22:58 -0300

    mod

--- src/components/Form-Login/form-login.tsx
+++ src/components/Form-Login/form-login.tsx
@@ -1,114 +1,19 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ButtonDark from "../Button-dark/button-dark";
 import ButtonLight from "../Button-light/button-light";
 import Input from "../Input/input";
 import TitleForm from "../Title-form/title-form";
-import api from "../../config/config";
 import useInputChange from "../../hook/useInputChange";
-import useValidation from "../../hook/useValidation";
 
-import { ToastContainer, toast } from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Loading from "../Loading/loading";
-import { useContext } from "react";
-import { UserAutenticado } from "../../context/authContext";
+import useLogin from "../../hook/useLogin";
 
 export default function FormLogin() {
 
-    const navigate = useNavigate()
     const { email, senha, handleEmailValue, handleSenhaValue } = useInputChange()
-    const { validation, setValidation } = useValidation()
-
-    const {signIn} = useContext(UserAutenticado)
-
-
-    async function handleSubmit(e: any) {
-        e.preventDefault()
-
-        setValidation({ ...validation, loading: true })
-
-        const data = {
-            email,
-            senha
-        }
-
-        await api.post("/login", data, {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }).then((response) => {
-            console.log(response)
-
-            if (response.status === 201) {
-                setValidation({
-                    type: "success",
-                    message: "sucess",
-                    loading: false
-                })
-                navigate("/home")
-                localStorage.setItem("tokenUser", JSON.stringify(response.data.accessToken))
-
-                signIn(true)
-            }
-        }).catch((error) => {
-            console.log(error)
-            if (error.response.status === 400) {
-
-                setValidation({ ...validation, loading: false })
-
-                toast.error(error.response.data.message, {
-                    position: "bottom-center",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-
-                if (senha.length < 6) {
-                    console.log(error.response.data.message[0])
-                    setValidation({
-                        type: "senha-length",
-                        message: "A senha deve conter no mínimo 6 caracteres.",
-                        loading: false
-                    })
-
-                    toast.error("A senha deve conter no mínimo 6 caracteres.", {
-                        position: "bottom-center",
-                        autoClose: 5000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "colored",
-                    })
-
-                }
-            } else if (error.response.status === 401) {
-                toast.error("Email ou senha incorretos.", {
-                    position: "bottom-center",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                })
-
-                setValidation({ ...validation, loading: false })
-            } else {
-                setValidation({
-                    type: "error",
-                    message: "Erro: tente mais tarde.",
-                    loading: false
-                })
-            }
-        })
-    }
+    const {handleSubmit, validation}  = useLogin()
 
     return (
         <>
