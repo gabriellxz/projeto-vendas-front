65ae8bda1985943a4cd12121a91da972ec857b44
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-01-27 22:23:19 -0300

    hook register user

---
+++ src/hook/useCadastro.ts
@@ -0,0 +1,134 @@
+import { toast } from "react-toastify"
+import api from "../config/config"
+import useInputChange from "./useInputChange"
+import useValidation from "./useValidation"
+import { useNavigate } from "react-router-dom"
+
+export default function useCadastro() {
+
+    const navigate = useNavigate()
+    const {
+
+        telefoneData,
+        email,
+        senha,
+        nome,
+        genero
+
+    } = useInputChange()
+    const { validation, setValidation } = useValidation()
+
+    async function handleSubmit(e: any) {
+
+        setValidation({ ...validation, loading: true })
+
+        e.preventDefault()
+        const Telefone = telefoneData.ddd + telefoneData.Telefone
+
+        console.log(email)
+        console.log(senha)
+        console.log(Telefone)
+        console.log(nome)
+        console.log(genero)
+
+
+        const data = {
+            email,
+            senha,
+            nome,
+            Telefone,
+            genero
+        }
+
+        await api.post("/registrar", data, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((response) => {
+            console.log(response)
+
+            if (response.status === 201) {
+                navigate("/")
+
+                toast.success("Usuário cadastrado com sucesso!", {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                })
+
+                setValidation({
+                    type: "sucess",
+                    message: "Usuário cadastrado com sucesso!",
+                    loading: false
+                })
+            }
+        }).catch((error) => {
+            console.log(error)
+            if (error.response.status === 400) {
+
+                setValidation({ ...validation, loading: false })
+
+                toast.error("Preencha os campos corretamente.", {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                })
+
+                if (senha.length < 6) {
+                    console.log(error.response.data.message[0])
+                    setValidation({
+                        type: "senha-length",
+                        message: "A senha deve conter no mínimo 6 caracteres.",
+                        loading: false
+                    })
+
+                    toast.error("A senha deve conter no mínimo 6 caracteres.", {
+                        position: "bottom-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
+                    })
+
+                }
+            } else if (error.response.status === 404) {
+                toast.error("Este usuário já existe!", {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                })
+
+                setValidation({ ...validation, loading: false })
+            } else {
+                setValidation({
+                    type: "error",
+                    message: "Erro: tente mais tarde.",
+                    loading: false
+                })
+            }
+        })
+    }
+
+    return {
+        handleSubmit,
+        validation
+    }
+}
