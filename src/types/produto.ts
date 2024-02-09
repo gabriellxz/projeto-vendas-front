a97980727987a96505a254fa643c6e9bee45c28e
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-02-08 21:30:42 -0300

    create type product

---
+++ src/types/produto.ts
@@ -0,0 +1,9 @@
+export default interface ProdutosDTO {
+    id_produto: number;
+    nome_produto: string;
+    preco: number;
+    descricao: string;
+    estoque: number;
+    createdAt: string;
+    updatedAt: string;
+}
