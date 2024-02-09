c4c1252d7b5946bf4390448513eabceb35faa236
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-02-08 21:27:55 -0300

    background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    fontFamily: {
-      jura: ['Jura', 'sans-serif']
-    }
+    extend: {
+      fontFamily: {
+        jura: ['Jura', 'sans-serif']
+      },
+      backgroundImage: {
+        "image-home-1": "url('/public/imagem-home-1.jpeg')"
+      }
+    },
   },
   plugins: [],
 }
