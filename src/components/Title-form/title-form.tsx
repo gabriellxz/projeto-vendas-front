2b629a56875a88619506e413c18ab76c1a93e781
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-02-15 16:48:46 -0300

    new color

--- src/components/Title-form/title-form.tsx
+++ src/components/Title-form/title-form.tsx
@@ -5,7 +5,7 @@ interface Props {
 export default function TitleForm(props:Props) {
     return(
         <>
-            <h1 className="text-3xl font-bold uppercase">{props.text}</h1>
+            <h1 className="text-3xl font-bold uppercase text-greenEco-100">{props.text}</h1>
         </>
     )
 }
