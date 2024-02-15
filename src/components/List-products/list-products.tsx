b5f7cf618a2e6330e6f5e6a927e43f80fd81cb9d
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-02-15 16:49:30 -0300

    create product component

--- src/components/List-products/list-products.tsx
+++ src/components/List-products/list-products.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react"
 import ProdutosDTO from "../../types/produto"
 import api from "../../config/config"
+import imageHome1 from '../../../public/imagem-home-1.jpeg'
+import imageHome2 from '../../../public/imagem-home-2.jpeg'
+import imageHome3 from '../../../public/imagem-home-3.jpeg'
 
 export default function ListProduct() {
 
@@ -29,17 +32,23 @@ export default function ListProduct() {
 
     return (
         <div>
-            <div>
-                <div className="">
-
-                </div>
+            <div className={`
+                hidden
+                xl:flex xl:justify-center
+            `}>
+                <img src={imageHome1} alt="image-home" className="max-w-[489px] w-full" />
+                <img src={imageHome2} alt="image-home" className="max-w-[489px] w-full" />
+                <img src={imageHome3} alt="image-home" className="max-w-[489px] w-full" />
             </div>
             <div>
-                {
-                    product.map((p: ProdutosDTO) => (
-                        <p key={p.id_produto}>{p.nome_produto}</p>
-                    ))
-                }
+                <div className="m-10">
+                    <div className="text-center text-greenEco-200">
+                        <h1 className="uppercase font-kaisei text-5xl">elevando sua rotina de cuidados diários</h1>
+                        <p className="font-inter text-xl">
+                            Descubra o Segredo para uma Beleza Autentica
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
