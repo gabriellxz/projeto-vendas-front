c5f3e5f8d3eb25ceda611a464e867dfed025a0c6
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-02-15 22:26:35 -0300

    slide images

--- src/components/List-products/list-products.tsx
+++ src/components/List-products/list-products.tsx
@@ -4,11 +4,26 @@ import api from "../../config/config"
 import imageHome1 from '../../../public/imagem-home-1.jpeg'
 import imageHome2 from '../../../public/imagem-home-2.jpeg'
 import imageHome3 from '../../../public/imagem-home-3.jpeg'
+import { Link } from "react-router-dom"
 
 export default function ListProduct() {
 
     const token = localStorage.getItem("tokenUser")
     const [product, setProduct] = useState<ProdutosDTO[]>([])
+    const [imageSlide, setImageSlide] = useState(0)
+    const images = [
+        imageHome1,
+        imageHome2,
+        imageHome3
+    ]
+
+    useEffect(()  => {
+        const intervalId = setInterval(() => {
+            setImageSlide((prevIndex) => (prevIndex + 1) % images.length)
+        }, 5000)
+
+        return () => clearInterval(intervalId)
+    }, [])
 
     useEffect(() => {
         async function getProducts() {
@@ -33,13 +48,22 @@ export default function ListProduct() {
 
     return (
         <div>
+            {/* IMAGENS VISÍVEIS PARA TELAS GRANDES */}
             <div className={`
-                hidden
+                hidden mt-7
                 xl:flex xl:justify-center
             `}>
                 <img src={imageHome1} alt="image-home" className="max-w-[489px] w-full" />
                 <img src={imageHome2} alt="image-home" className="max-w-[489px] w-full" />
                 <img src={imageHome3} alt="image-home" className="max-w-[489px] w-full" />
+            </div>
+
+            {/* IMAGENS VISÍVEIS PARA TELAS PEQUENAS */}
+            <div className={`
+                xl:hidden mt-7
+                flex justify-center
+            `}>
+                <img src={images[imageSlide]} alt="image-home" className="max-w-[489px] w-full" />
             </div>
             <div>
                 <div className="m-10">
@@ -48,6 +72,9 @@ export default function ListProduct() {
                         <p className="font-inter text-xl">
                             Descubra o Segredo para uma Beleza Autentica
                         </p>
+                        <Link to={""}>
+                            Nossos Produtos
+                        </Link>
                     </div>
                 </div>
             </div>
