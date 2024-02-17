9d3e1cbe29c9f85c1a56a0d3fdda276957481a7b
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-02-17 16:15:29 -0300

    navbar mobile

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -4,30 +4,67 @@ import SearchIcon from "../../svg/search-icon";
 import UserIcon from "../../svg/user-icon";
 import Logo_ecogreen from "../../../public/Cosméticos.svg"
 import ButtonLogout from "../../svg/button-logout";
+import NavBarIcon from "../../svg/navbar-icon";
+import { useState } from "react";
+import CloseNavBar from "../../svg/closeNavbar";
 
 export default function Header() {
 
-    const token =  localStorage.getItem("tokenUser")
+    const token = localStorage.getItem("tokenUser")
+    const [open, setOpen] = useState<boolean>(false)
 
     return (
         <div>
-            <header className="flex items-center justify-end p-5">
+            {/* VISÍVEL PARA TELAS GRANDES */}
+            <header className={`
+                hidden p-5
+                lg:flex lg:items-center lg:justify-end
+            `}>
                 <div className="flex justify-between max-w-[800px] w-full">
                     <div className="">
-                        <img src={Logo_ecogreen} className="w-[100px]" alt="logo_ecogreen"/>
+                        <img src={Logo_ecogreen} className="w-[100px]" alt="logo_ecogreen" />
                     </div>
                     <div className="flex items-center gap-5">
                         <SearchIcon />
                         <UserIcon />
                         <FavoriteIcon />
                         <BagIcon />
-                        {token ? <ButtonLogout/> : ""}
+                        {token ? <ButtonLogout /> : ""}
                     </div>
                 </div>
             </header>
+
+            {/* VISÍVEL PARA TELAs PEQUENAS */}
+            <header className={`
+                flex items-center justify-between p-5
+                lg:hidden
+            `}>
+                <div className="flex items-center justify-between max-w-[800px] w-full">
+                    <div className="">
+                        <img src={Logo_ecogreen} className="w-[70px]" alt="logo_ecogreen" />
+                    </div>
+                    {open ? <CloseNavBar handleNavBar={() => setOpen(!open)}/> : <NavBarIcon handleNavBar={() => setOpen(!open)} />}
+                    {
+                        open &&
+                        <div className={`
+                            flex items-center gap-5
+                        `}>
+                            <SearchIcon />
+                            <UserIcon />
+                            <FavoriteIcon />
+                            <BagIcon />
+                            {token ? <ButtonLogout /> : ""}
+                        </div>
+                    }
+
+                </div>
+            </header>
+
             <div className="flex justify-center px-[58px]">
                 <div className="bg-zinc-400 p-[0.5px] w-full"></div>
             </div>
+
+
         </div>
     )
 }
---
+++ src/svg/navbar-icon.tsx
@@ -0,0 +1,13 @@
+interface Props {
+    handleNavBar: () => void
+}
+
+export default function NavBarIcon(props:Props) {
+    return (
+        <div onClick={props.handleNavBar}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+        </div>
+    )
+}
