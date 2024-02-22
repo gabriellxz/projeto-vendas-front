05dd547f8b86809423e041c2010b4583f0cd6c38
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-02-21 22:52:12 -0300

    delete react slick mobile

--- src/components/Slides-mini-card/slides-mini-card.tsx
+++ src/components/Slides-mini-card/slides-mini-card.tsx
@@ -1,6 +1,3 @@
-import Slider from "react-slick";
-import NextArrow from "../NextArrow/next-arrow";
-import PrevArrow from "../PrevArrow/prev-arrow";
 import useListProduct from "../../hook/useListProduct";
 import ProdutosDTO from "../../types/produto";
 import MiniCard from "../Mini-card/mini-card";
@@ -9,51 +6,19 @@ export default function SlidesMiniCard() {
 
     const { product } = useListProduct()
 
-    const settings2 = {
-        className: "center",
-        centerMode: true,
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        responsive: [
-            {
-                breakpoint: 640,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 500,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-        ]
-    }
-
     return (
         <>
             {/* TELAS PEQUENAS */}
             <div className={`
                             sm:hidden
                         `}>
-                <Slider {...settings2}>
+                <div className="overflow-scroll flex">
                     {
                         product.map((product: ProdutosDTO) => (
                             <MiniCard key={product.id_produto} iProduto={product} />
                         ))
                     }
-                </Slider>
+                </div>
             </div>
 
             {/* TELAS GRANDES */}
