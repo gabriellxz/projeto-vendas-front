95abed59e0330a51d7c63b032df5b9f91e38fb22
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-03-04 21:42:19 -0300

    props function btn

--- src/components/Button-dark/button-dark.tsx
+++ src/components/Button-dark/button-dark.tsx
@@ -1,14 +1,14 @@
 interface Props {
     text: string;
     login?: () => void;
-    handleLogin?: () => void;
+    propsBtn?: () => void;
     filePost?: (produtoId:number) => void;
 }
 
 export default function ButtonDark(props:Props) {
     return (
         <>
-            <button className="uppercase bg-greenEco-200 w-full text-white p-2" onClick={props.handleLogin}>{props.text}</button>
+            <button className="uppercase bg-greenEco-200 w-full text-white p-2" onClick={props.propsBtn}>{props.text}</button>
         </>
     )
 }
