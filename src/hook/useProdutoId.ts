feceb3d8adac0065bf48beaeeab7a9be0b45e68b
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-03-09 10:50:22 -0300

    useProdutoId

--- src/hook/useProdutoId.ts
+++ src/hook/useProdutoId.ts
@@ -2,36 +2,47 @@ import * as productService from '../service/produto-service'
 import { useEffect, useState } from "react";
 import ProdutosDTO from "../types/produto";
 import { useParams } from "react-router-dom"
+import { AxiosResponse } from 'axios';
+import api from '../config/config';
 
 export default function useProdutoId() {
 
     const params = useParams()
     const [produto, setProduto] = useState<ProdutosDTO>()
+    const [produtoId, setProdutoId] = useState<number>()
     const [loading, setLoading] = useState<boolean>(false)
-    const [idProduto, setIdProduto] = useState<number>()
     const token = localStorage.getItem("tokenUser")
 
     useEffect(() => {
+        async function produtoById() {
 
-        setLoading(true)
+            setLoading(true)
 
-        if (token) {
-            productService.findById(Number(params.idProduto), token)
-                .then((response) => {
+            try {
+                if (token) {
+                    const response: AxiosResponse = await api.get(`/Product/${Number(params.idProduto)}`, {
+                        headers: {
+                            "Authorization": "Bearer " + JSON.parse(token)
+                        }
+                    })
+
+                    setLoading(false)
                     console.log(response.data)
                     setProduto(response.data)
-                    setIdProduto(response.data.id_produto)
-                    setLoading(false)
-                }).catch((err) => {
-                    console.log(err)
-                    setLoading(false)
-                })
+                    console.log("id do produto: "+ response.data.id_produto)
+                }
+            } catch (error) {
+                setLoading(false)
+                console.log(error)
+            }
         }
+
+        produtoById()
     }, [])
 
     return {
         produto,
         loading,
-        idProduto
+        produtoId
     }
 }
