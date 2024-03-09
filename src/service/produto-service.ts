c928662df85108da1b1b5b173678a604bcc8ade5
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-03-09 10:52:12 -0300

    excluindo services

--- src/service/produto-service.ts
+++
@@ -1,9 +0,0 @@
-import api from "../config/config";
-
-export function findById(id: number, token: string) {
-    return api.get(`/Product/${id}`, {
-        headers: {
-            "Authorization": `Bearer ${JSON.parse(token)}`
-        }
-    })
-}
