928fd8f5bfb87a82297ef705bac56de41f0e669d
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-03-19 14:15:07 -0300

    adicionando endereços

--- src/components/Sumario-compras/sumario-compras.tsx
+++ src/components/Sumario-compras/sumario-compras.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { CartType } from "../../types/cart"
 import Moeda from "../../utils/moeda";
 import ButtonDark from "../Button-dark/button-dark";
+import useEndereco from "../../hook/useEndereco";
+import Endereco from "../../types/endereco";
+import { useNavigate } from "react-router-dom";
 
 interface PropsSumario {
     iCart: CartType[];
@@ -9,6 +12,8 @@ interface PropsSumario {
 
 export default function SumarioCompras(props: PropsSumario) {
 
+    const navigate = useNavigate()
+    const { endereco } = useEndereco()
     const [subTotal, setSubTotal] = useState(0)
 
     useEffect(() => {
@@ -21,6 +26,7 @@ export default function SumarioCompras(props: PropsSumario) {
         setSubTotal(total)
     }, [props.iCart])
 
+
     return (
         <>
             <div className="mb-[67px]">
@@ -32,9 +38,22 @@ export default function SumarioCompras(props: PropsSumario) {
                     <span className="font-bold">{Moeda.formatar(subTotal)}</span>
                 </div>
                 <div className="bg-zinc-400 p-[0.5px] w-full"></div>
-                <div>
-                    <ButtonDark text="Prosseguir com a compra"/>
-                </div>
+                <form>
+                    {
+                        endereco.length < 1 ? <span className="text-red-600">Você não tem endereço.</span>
+                            :
+                            <select className="border border-1 border-black outline-none p-2 w-full mb-5" required>
+                                {
+                                    endereco.map((item: Endereco) => (
+                                        <option key={item.endereco_id}>{`${item.bairro}, ${item.cidade}, ${item.estado}`}</option>
+                                    ))
+                                }
+                            </select>
+                    }
+                    {
+                        endereco.length < 1 ? <ButtonDark text="Adicionar endereço" propsBtn={() => {navigate("/home/criar-endereço")}}/> : <ButtonDark text="Prosseguir com a compra"/>
+                    }
+                </form>
             </div>
         </>
     )
