d01b72df5efab8cb0a1f105a16019afa3ccb8951
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-03-19 14:13:58 -0300

    props de delereProductCart

--- src/components/Button-light/button-light.tsx
+++ src/components/Button-light/button-light.tsx
@@ -1,11 +1,12 @@
 interface Props {
     text: string;
+    removeProductCart?: (produtoId:number) => void;
 }
 
 export default function ButtonLight(props:Props) {
     return (
         <>
-            <button className="p-2 border border-1 border-black w-full">{props.text}</button>
+            <button className="p-2 border border-1 border-black w-full" onClick={() => props.removeProductCart}>{props.text}</button>
         </>
     )
 }
