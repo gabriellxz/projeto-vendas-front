73516243a41379cc4f9e187673f593e64dffa05f
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-03-19 14:16:59 -0300

    adicionando tipo "Endereço"

---
+++ src/types/endereco.ts
@@ -0,0 +1,11 @@
+export default interface Endereco {
+    CEP: string;
+    numero: number;
+    complemento: string;
+    ponto_de_referencia: string;
+    bairro: string;
+    estado: string;
+    cidade: string;
+    telefone_contato: string;
+    endereco_id: number;
+}
