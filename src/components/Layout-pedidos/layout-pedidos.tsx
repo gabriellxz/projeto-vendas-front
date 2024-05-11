6ced2b823d478e02c6766a1d1f3b472614cca643
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-05-11 20:15:59 -0300

    refactor: chamando componente através de props

--- src/components/Layout-pedidos/layout-pedidos.tsx
+++ src/components/Layout-pedidos/layout-pedidos.tsx
@@ -1,25 +1,20 @@
 import FilterIcon from "../../svg/filter-icon";
-import Pedidos from "../../types/pedidos";
-import Loading from "../Loading/loading";
 import usePedidos from "../../hook/usePedidos"
 import { useState } from "react"
-import { Link, useLocation } from "react-router-dom";
-import useListProduct from "../../hook/useListProduct";
-import ProdutosDTO from "../../types/produto";
+import Loading from "../Loading/loading";
 
 interface PropsLayout {
     titleLayout: string;
-    th1: string;
-    th2: string;
-    th3: string;
+    th1?: string;
+    th2?: string;
+    th3?: string;
     styleTable: string;
+    component?: any;
 }
 
 export default function LayoutPedidos(props: PropsLayout) {
 
-    const location = useLocation()
-    const { loading, orderUser } = usePedidos()
-    const { product } = useListProduct()
+    const { loading } = usePedidos()
     const [openModal, setOpenModal] = useState<boolean>(false)
 
     return (
@@ -60,7 +55,7 @@ export default function LayoutPedidos(props: PropsLayout) {
                             </div>
                         </div>
                     }
-                    <table className="w-full">
+                    <table className={`w-full`}>
                         <thead className="w-full border-b border-gray-500">
                             <tr className="w-full flex justify-between pb-3">
                                 <th className="w-full text-left">{props.th1}</th>
@@ -71,40 +66,7 @@ export default function LayoutPedidos(props: PropsLayout) {
                         <tbody className="w-full font-bold">
                             <div className="overflow-y-scroll h-[300px]">
                                 {
-                                    location.pathname !== "/dashboard/produto-e-estoque" ? (
-                                        loading ? <Loading /> :
-                                            orderUser.map((order: Pedidos) => {
-                                                const data = new Date(order.createdAt)
-
-                                                const dia = String(data.getDate()).padStart(2, "0")
-                                                const mes = String(data.getMonth() + 1).padStart(2, "0")
-                                                const ano = data.getFullYear()
-
-                                                return (
-                                                    <Link to={`/dashboard/detalhes-de-pedidos/${order.userId}`}>
-                                                        <tr className="w-full flex justify-between items-center mt-5" key={order.id}>
-                                                            <td className="w-full text-left">{order.users.nome}</td>
-                                                            <td className="w-full text-center">{`${dia}-${mes}-${ano}`}</td>
-                                                            {
-                                                                order.Delivered !== false ? <td className="w-full text-center bg-green-600 text-white rounded-2xl py-2">Entregue</td> : <td className="w-full text-center bg-orange-400 text-white rounded-2xl py-2">Envio pendente</td>
-                                                            }
-                                                        </tr>
-                                                    </Link>
-                                                )
-                                            })
-                                    ) : (
-                                        loading ? <Loading /> :
-                                            product.map((p: ProdutosDTO) => (
-                                                <tr className={`w-full flex justify-between items-center mt-5`} key={p.id_produto}>
-                                                    <td className="w-full text-left">{p.nome_produto}</td>
-                                                    <td className="w-full text-center">{p.preco}</td>
-                                                    <td className="w-full text-center">{p.estoque}</td>
-                                                </tr>
-                                            ))
-                                    )
-                                }
-                                {
-
+                                   loading ? <Loading/> : props.component 
                                 }
                             </div>
                         </tbody>
