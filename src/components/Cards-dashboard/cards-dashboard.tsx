a0bbd3ae8b089bc1911ae8210146b69c53d21bfa
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-05-13 19:37:35 -0300

    passando propriedade com função de renderização

--- src/components/Cards-dashboard/cards-dashboard.tsx
+++ src/components/Cards-dashboard/cards-dashboard.tsx
@@ -5,13 +5,14 @@ interface PropsCard {
     orderUserLength?: number;
     styleCard?: string;
     link?: () => void;
+    editProductFunction?: () => void;
 }
 
 export default function CardDashboard(props:PropsCard) {
     
     return (
         <>
-            <div className="flex max-w-[380px] w-full bg-white px-3 py-5 gap-[13px] rounded-[20px]">
+            <div className="flex max-w-[380px] w-full bg-white px-3 py-5 gap-[13px] rounded-[20px]" onClick={props.editProductFunction}>
                 <div className="flex justify-center items-center bg-blue-400 max-w-[100px] w-full h-[100px] rounded-2xl">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-[45px] h-[45px] text-blue-800">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
