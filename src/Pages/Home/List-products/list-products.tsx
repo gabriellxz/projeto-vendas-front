b33c5b6eaf56c71e303404830de3094f33f52a85
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-05-18 16:02:51 -0300

    add gsap

--- src/Pages/Home/List-products/list-products.tsx
+++ src/Pages/Home/List-products/list-products.tsx
@@ -3,17 +3,120 @@ import 'slick-carousel/slick/slick-theme.css';
 import SlidesHome from "../../../components/Slides-home/slides-home"
 import Catalog from "../../../components/Catalog/catalog"
 import SlidesMiniCard from '../../../components/Slides-mini-card/slides-mini-card';
-// import SlidesMiniCard from "../../../components/Slides-mini-card/slides-mini-card";  
+// import SlidesMiniCard from "../../../components/Slides-mini-card/slides-mini-card"; 
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { useLayoutEffect, useRef } from 'react';
 
 export default function ListProduct() {
+
+    const el: any = useRef()
+    const tl: any = useRef()
+
+    useLayoutEffect(() => {
+        gsap.registerPlugin(ScrollTrigger)
+        gsap.context(() => {
+            const mm = gsap.matchMedia();
+
+            mm.add("(min-width: 768px)", () => {
+                tl.current = gsap.timeline({
+                    scrollTrigger: {
+                        trigger: ".box-text-title",
+                        scrub: true,
+                        end: "bottom 800px",
+                        start: "top 100px"
+                    }
+                })
+                    .fromTo("#text-1", {
+                        opacity: 0,
+                        y: 160
+                    }, {
+                        opacity: 1,
+                        y: 0
+                    })
+                    .fromTo("#text-2", {
+                        opacity: 0,
+                        y: 160
+                    }, {
+                        opacity: 1,
+                        y: 0
+                    })
+            });
+
+            mm.add("(max-width: 767px)", () => {
+                tl.current = gsap.timeline({
+                    scrollTrigger: {
+                        trigger: ".box-text-title",
+                        scrub: true,
+                        end: "bottom 600px",
+                        start: "top 80px"
+                    }
+                })
+                    .fromTo("#text-1", {
+                        opacity: 0,
+                        y: 100
+                    }, {
+                        opacity: 1,
+                        y: 0
+                    })
+                    .fromTo("#text-2", {
+                        opacity: 0,
+                        y: 100
+                    }, {
+                        opacity: 1,
+                        y: 0
+                    })
+            });
+
+            return () => {
+                mm.revert();
+            };
+        }, el)
+
+        return () => {
+            gsap.killTweensOf(".box-text-title")
+        }
+
+    }, [])
+
+    useLayoutEffect(() => {
+        gsap.registerPlugin(ScrollTrigger)
+        const mm = gsap.matchMedia();
+
+        mm.add("(min-width: 768px)", () => {
+            gsap.to(".slides", {
+                x: 0,
+                scrollTrigger: {
+                    scrub: true,
+                }
+            })
+        });
+
+        mm.add("(max-width: 767px)", () => {
+            gsap.to(".slides", {
+                x: 0,
+                scrollTrigger: {
+                    scrub: true,
+                    end: "bottom 2000px",
+                }
+            })
+        });
+
+        return () => {
+            mm.revert();
+            gsap.killTweensOf(".slides")
+        }
+
+    }, [])
+
     return (
         <div>
             <SlidesHome />
             <div>
-                <div className="mt-[56px] sm:m-10">
-                    <div className="text-center text-greenEco-200">
-                        <h1 className="uppercase font-kaisei text-[13px] sm:text-5xl">elevando sua rotina de cuidados diários</h1>
-                        <p className="font-inter text-[12px] sm:text-xl">
+                <div className="mt-[56px] sm:m-10 box-text-title">
+                    <div className="text-center text-greenEco-200" ref={el}>
+                        <p className="uppercase font-kaisei text-[13px] sm:text-5xl" id="text-1">elevando sua rotina de cuidados diários</p>
+                        <p className="font-inter text-[12px] sm:text-xl" id="text-2">
                             Descubra o Segredo para uma Beleza Autentica
                         </p>
                     </div>
@@ -25,6 +128,8 @@ export default function ListProduct() {
                         flex flex-col items-center justify-center
                         lg:flex lg:flex-row
                         w-full bg-whiteEco-200 p-10 gap-[100px] mt-7
+                        translate-x-[-100%]
+                        slides
                     `}>
                         <div className="flex justify-center items-center flex-col max-w-[260px] gap-7">
                             <div className="flex justify-center flex-col gap-2">
@@ -44,7 +149,7 @@ export default function ListProduct() {
                             <span className='uppercase text-2xl'>nossos produtos</span>
                         </div>
                         <div className='flex justify-center'>
-                            <Catalog reload={() => {}} />
+                            <Catalog reload={() => { }} />
                         </div>
                     </div>
                 </div>
--- src/components/Catalog/catalog.tsx
+++ src/components/Catalog/catalog.tsx
@@ -1,30 +1,64 @@
+import { useRef, useLayoutEffect } from "react";
 import useListProduct from "../../hook/useListProduct"
 import ProdutosDTO from "../../types/produto"
 import CardProduct from "../Card-Product/card-product"
 import Loading from "../Loading/loading"
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 interface Props {
     reload: () => void;
 }
 
-export default function Catalog(props:Props) {
+export default function Catalog(props: Props) {
 
     const { product, loading } = useListProduct()
+    const el: any = useRef()
+    const tl: any = useRef()
+
+    useLayoutEffect(() => {
+
+        gsap.registerPlugin(ScrollTrigger)
+        gsap.context(() => {
+            tl.current = gsap.timeline({
+                scrollTrigger:{
+                    trigger: ".card-item",
+                    scrub: true,
+                    // markers: true,
+                    end: "bottom 400px"
+                }
+            })
+            .fromTo(".card-item", {
+                opacity: 0,
+                y: 160
+            }, {
+                opacity: 1,
+                y: 0
+            })
+        }, el)
+
+        return () => {
+            gsap.killTweensOf(".card-item")
+        }
+
+    }, [])
 
     return (
-        <div className="sm:grid sm:grid-cols-2 sm:gap-7 lg:grid-cols-3 grid grid-cols-2 gap-5 mt-5 pt-10 p-10">
+        <div ref={el}>
+            <div className="sm:grid sm:grid-cols-2 sm:gap-7 lg:grid-cols-3 grid grid-cols-2 gap-5 mt-5 pt-10 p-10 card-item">
 
-            {loading ? (
-                <Loading />
-            ) : (
-                product.length > 0 ? (
-                    product.map((product: ProdutosDTO) => (
-                        <CardProduct key={product.id_produto} iProduto={product} reload={props.reload}/>
-                    ))
+                {loading ? (
+                    <Loading />
                 ) : (
-                    <p>Não existem produtos...</p>
-                )
-            )}
+                    product.length > 0 ? (
+                        product.map((product: ProdutosDTO) => (
+                            <CardProduct key={product.id_produto} iProduto={product} reload={props.reload} />
+                        ))
+                    ) : (
+                        <p>Não existem produtos...</p>
+                    )
+                )}
+            </div>
         </div>
     )
 }
