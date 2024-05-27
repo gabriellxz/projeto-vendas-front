af6be602368e7ed90e1c07567805d41fe7ba6ddf
Gabriel <gabrielsilvaoliveira521@gmail.com>
2024-05-27 14:50:00 -0300

    update

--- src/Pages/Home/Order-user/order-user.tsx
+++ src/Pages/Home/Order-user/order-user.tsx
@@ -137,8 +137,9 @@ export default function OrderUser() {
             }
             <div className="w-full">
                 {
-                    delivered !== false ? <span className="hidden">
-                        <ButtonDark text="Confirmar entrega" propsBtn={confirmedOrder} />
+                    delivered !== false ? <span className="uppercase bg-greenEco-200 w-full text-white p-2">
+                        {/* <ButtonDark text="Confirmar entrega" propsBtn={confirmedOrder} /> */}
+                        Seu pedido foi entregue
                     </span> : <ButtonDark text="Confirmar entrega" propsBtn={confirmedOrder} />
                 }
             </div>
