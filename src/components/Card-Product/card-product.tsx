779e6526e71bb69cbde492c9f711f89658b7270c
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2024-09-22 18:23:49 -0300

    refactor: novo estilo de card

--- src/components/Card-Product/card-product.tsx
+++ src/components/Card-Product/card-product.tsx
@@ -1,10 +1,7 @@
 import { Link } from 'react-router-dom'
 import ProdutosDTO from '../../types/produto'
 import Moeda from '../../utils/moeda';
-import { ShoppingCartIcon } from '@heroicons/react/16/solid';
 import useCart from '../../hook/useCart';
-import { CartOrderUser } from '../../types/cart';
-import { CartIcon } from '../../svg/cart-icon';
 import Loading from '../Loading/loading';
 // import imgProduct from '../../assets/produto-1.webp'
 
@@ -15,47 +12,20 @@ interface PropsProduto {
 
 export default function CardProduct(props: PropsProduto) {
 
-    const { handleAddCart, cart, loadingCart } = useCart();
-
-    const isCart = cart.some((c: CartOrderUser) => c.produtoId === props.iProduto.id_produto);
+    const { handleAddCart, loadingCart } = useCart();
 
     return (
-        <div className='bg-zinc-100 p-2'>
-            <Link to={`/home/detalhes-produtos/${props.iProduto.id_produto}`} onClick={props.reload}>
-                <div className="sm:max-w-[630px] sm:w-full sm:h-[697px]">
-                    <div>
-                        <img src={props.iProduto.imagem && props.iProduto.imagem[0] && props.iProduto.imagem[0].url} alt="" className='w-[200px] h-[200px] sm:w-[630px] sm:h-[630px]' />
-                    </div>
-                    <div>
-                        <div className='flex justify-between py-1'>
-                            <h2 className="text-xl text-greenEco-300 font-bold text-[15px]"
-                                style={{ maxWidth: '200px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
-                            >{props.iProduto.nome_produto}</h2>
-                            <div className='flex gap-5'>
-                                <span className='font-bold text-2xl text-green-700'>{Moeda.formatar(props.iProduto.preco)}</span>
-                            </div>
-                        </div>
-                        <p className="text-greenEco-200 text-[12px]"
-                            style={{ maxWidth: '200px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
-                        >
-                            {props.iProduto.descricao}
-                        </p>
-                    </div>
-                </div>
+        <div className={`text-center`}>
+            <Link to={`/detalhes-produtos/${props.iProduto.id_produto}`}>
+                <img src={props.iProduto.imagem && props.iProduto.imagem[0] && props.iProduto.imagem[0].url} alt="" className='py-5 max-w-[300px] h-[300px] w-full' />
             </Link>
-            <span className='flex justify-end cursor-pointer'>
-                {isCart ? (
-                    <>
-                        {loadingCart ? <Loading /> : <ShoppingCartIcon className='w-[30px]' onClick={() => handleAddCart(props.iProduto.id_produto)} />}
-                    </>
-                ) : (
-                    <>
-                        {loadingCart ? <Loading /> : <span onClick={() => handleAddCart(props.iProduto.id_produto)}>
-                            <CartIcon />
-                        </span>}
-                    </>
-                )}
-            </span>
+            <Link to={`/detalhes-produtos/${props.iProduto.id_produto}`}>{props.iProduto.nome_produto}</Link>
+            <p className={`font-bold ${props.iProduto.oferta ? "text-green-600" : ""}`}>{Moeda.formatar(props.iProduto.preco)}</p>
+            <div className='flex justify-center'>
+                {
+                    loadingCart ? <Loading /> : <button className="p-2 border border-1 border-black w-full" onClick={() => handleAddCart(props.iProduto.id_produto)}>Adicionar ao carrinho</button>
+                }
+            </div>
         </div>
     )
 }
--- src/components/Catalog/catalog.tsx
+++ src/components/Catalog/catalog.tsx
@@ -1,10 +1,7 @@
-import { useRef, useLayoutEffect } from "react";
 import useListProduct from "../../hook/useListProduct"
 import ProdutosDTO from "../../types/produto"
 import CardProduct from "../Card-Product/card-product"
 import Loading from "../Loading/loading"
-import { gsap } from "gsap"
-import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 interface Props {
     reload: () => void;
@@ -14,42 +11,42 @@ interface Props {
 export default function Catalog(props: Props) {
 
     const { product, loading } = useListProduct()
-    const el: any = useRef()
-    const tl: any = useRef()
+    // const el: any = useRef()
+    // const tl: any = useRef()
 
-    useLayoutEffect(() => {
+    // useLayoutEffect(() => {
 
-        gsap.registerPlugin(ScrollTrigger)
-        gsap.context(() => {
-            tl.current = gsap.timeline({
-                scrollTrigger: {
-                    trigger: ".card-item",
-                    scrub: true,
-                    // markers: true,
-                    end: "bottom 400px"
-                }
-            })
-                .fromTo(".card-item", {
-                    opacity: 0,
-                    y: 160
-                }, {
-                    opacity: 1,
-                    y: 0
-                })
-        }, el)
+    //     gsap.registerPlugin(ScrollTrigger)
+    //     gsap.context(() => {
+    //         tl.current = gsap.timeline({
+    //             scrollTrigger: {
+    //                 trigger: ".card-item",
+    //                 scrub: true,
+    //                 // markers: true,
+    //                 end: "bottom 400px"
+    //             }
+    //         })
+    //             .fromTo(".card-item", {
+    //                 opacity: 0,
+    //                 y: 160
+    //             }, {
+    //                 opacity: 1,
+    //                 y: 0
+    //             })
+    //     }, el)
 
-        return () => {
-            gsap.killTweensOf(".card-item")
-        }
+    //     return () => {
+    //         gsap.killTweensOf(".card-item")
+    //     }
 
-    }, [])
+    // }, [])
 
     const filterProduct = product.filter((product: ProdutosDTO) =>
         product.nome_produto.toLocaleLowerCase().includes(props.searchItem ? props.searchItem.toLowerCase() : "")
     )
 
     return (
-        <div ref={el}>
+        <div>
             <div className="sm:grid sm:grid-cols-2 sm:gap-7 lg:grid-cols-3 grid grid-cols-2 gap-5 mt-5 pt-10 p-10 card-item">
 
                 {loading ? (
