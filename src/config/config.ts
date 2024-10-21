d50699aebf2cb9fca028a132e9e763392df2143d
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2024-10-20 21:19:55 -0300

    fix: corringindo link da api

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_LINK
+    baseURL: "https://vendas-online-ruddy.vercel.app/"
 });
 
 export default api;
