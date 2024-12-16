23f19658435577986b88302207c89c27b997213f
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2024-12-16 15:33:19 -0300

    feat: iniciando função para editar perfil

--- src/Pages/Home/Perfil/perfil.tsx
+++ src/Pages/Home/Perfil/perfil.tsx
@@ -1,5 +1,5 @@
 import ButtonDark from "../../../components/Button-dark/button-dark";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserAutenticado } from "../../../context/authContext";
 import IconEdit from "../../../svg/icon-edit";
 import { motion } from "framer-motion"
@@ -8,6 +8,12 @@ import { Link } from "react-router-dom";
 export default function Perfil() {
 
     const { user } = useContext(UserAutenticado);
+
+    const [isEditTable, setIsEditTable] = useState(false)
+
+    function toggleEdit() {
+        setIsEditTable((prevState) => !prevState)
+    }
 
     return (
         <motion.div className="px-[55px] pt-[30px]"
@@ -29,28 +35,28 @@ export default function Perfil() {
                 <div className="flex items-center justify-between border border-b-zinc-500 px-[50px] py-2">
                     <span className="uppercase text-2xl">Dados pessoais</span>
                     <button className="bg-greenEco-200 p-2 rounded-[5px]">
-                        <IconEdit style="text-white w-[30px] h-[30px]" />
+                        <IconEdit style="text-white w-[30px] h-[30px]" onClickEdit={toggleEdit}/>
                     </button>
                 </div>
                 <div className="px-[40px] py-8">
                     <div className="flex flex-col sm:flex-row w-full gap-5 mb-[40px]">
                         <div className="flex flex-col sm:flex-row sm:items-center sm:gap-8 w-full">
                             <span>Nome</span>
-                            <input type="text" value={user?.name} className="border border-zinc-500 rounded-md w-full p-2" />
+                            <input disabled={!isEditTable} type="text" value={user?.name} className="border border-zinc-500 rounded-md w-full p-2" />
                         </div>
                         <div className="flex flex-col sm:flex-row sm:items-center sm:gap-8 w-full">
                             <span>Telefone</span>
-                            <input type="text" value={user?.telefone} className="border border-zinc-500 rounded-md w-full p-2" />
+                            <input disabled={!isEditTable} type="text" value={user?.telefone} className="border border-zinc-500 rounded-md w-full p-2" />
                         </div>
                     </div>
                     <div className="flex flex-col sm:flex-row w-full gap-5 mb-[40px]">
                         <div className="flex flex-col sm:flex-row sm:items-center sm:gap-8 w-full">
                             <span>Email</span>
-                            <input type="text" value={user?.email} className="border border-zinc-500 rounded-md w-full p-2" />
+                            <input disabled={!isEditTable} type="text" value={user?.email} className="border border-zinc-500 rounded-md w-full p-2" />
                         </div>
                         <div className="flex flex-col sm:flex-row sm:items-center sm:gap-8 w-full">
                             <span>CPF/CNPJ</span>
-                            <input type="text" value={user?.CPF} className="border border-zinc-500 rounded-md w-full p-2" />
+                            <input disabled={!isEditTable} type="text" value={user?.CPF} className="border border-zinc-500 rounded-md w-full p-2" />
                         </div>
                     </div>
                 </div>
