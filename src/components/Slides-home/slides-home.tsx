a280608df9faae179fd8069ff6d7dd4fced8e3cb
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2024-12-17 14:59:23 -0300

    refactor: alterando banners da home e retirando o slider

--- src/components/Slides-home/slides-home.tsx
+++ src/components/Slides-home/slides-home.tsx
@@ -1,79 +1,24 @@
-import Slider from 'react-slick';
-import imageHome1 from '../../../public/imagem-home-1.jpeg'
-import imageHome2 from '../../../public/imagem-home-2.jpeg'
-import imageHome3 from '../../../public/imagem-home-3.jpeg'
-import NextArrow from '../NextArrow/next-arrow';
+import background_mobile from "../../assets/background_mobile.png"
+import background_pc from "../../assets/background_pc.png"
 
 export default function SlidesHome() {
-
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 1000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        nextArrow: <NextArrow />,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
-
     return (
         <>
             <div className={`
                 hidden mt-7
                 lg:flex lg:justify-center
             `}>
-                <img src={imageHome1} alt="image-home" className={`
-                    max-w-[489px] w-full
-                    lg:max-w-[350px] 
-                    xl:max-w-[400px] 
-                `} />
-                <img src={imageHome2} alt="image-home" className={`
-                    max-w-[489px] w-full
-                    lg:max-w-[350px] 
-                    xl:max-w-[400px] 
-                `} />
-                <img src={imageHome3} alt="image-home" className={`
-                    max-w-[489px] w-full
-                    lg:max-w-[350px] 
-                    xl:max-w-[400px] 
+                <img src={background_pc} alt="image-home" className={`
+                    w-full
                 `} />
             </div>
 
             <div className={`
                 lg:hidden mt-7
             `}>
-                <Slider {...settings}>
-                    <img src={imageHome1} alt="" className="max-w-[489px] w-full" />
-                    <img src={imageHome2} alt="" className="max-w-[489px] w-full" />
-                    <img src={imageHome3} alt="" className="max-w-[489px] w-full" />
-                </Slider>
+                <img src={background_mobile} alt="image-home" className={`
+                    w-full
+                `} />
             </div>
         </>
     )
