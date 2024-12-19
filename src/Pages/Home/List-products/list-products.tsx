3e5f73ccada3931bc15f8f9d2da5a7f5141383dc
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2024-12-19 00:16:01 -0300

    style

--- src/Pages/Home/List-products/list-products.tsx
+++ src/Pages/Home/List-products/list-products.tsx
@@ -2,7 +2,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import SlidesHome from "../../../components/Slides-home/slides-home"
 import Catalog from "../../../components/Catalog/catalog"
-import SlidesMiniCard from '../../../components/Slides-mini-card/slides-mini-card';
+// import SlidesMiniCard from '../../../components/Slides-mini-card/slides-mini-card';
 import { useState, ChangeEvent } from 'react';
 import { motion, AnimatePresence } from "framer-motion"
 // import SlidesMiniCard from "../../../components/Slides-mini-card/slides-mini-card"; 
@@ -33,12 +33,12 @@ export default function ListProduct() {
                     <div className={`
                         flex flex-col items-center justify-center
                         lg:flex lg:flex-row
-                        w-full bg-whiteEco-200 p-10 gap-[100px] mt-7
+                        w-full bg-[#fae0a7] p-10 gap-[100px] mt-7
                     `}>
                         <div className="flex justify-center items-center flex-col max-w-[260px] gap-7">
                             <div className="flex justify-center flex-col gap-2">
                                 <h1 className="text-5xl">Oferta!</h1>
-                                <p className="text-whiteEco-100 text-justify">
+                                <p className="text-black text-[20px] text-justify">
                                     Confira nosso incriveis kits para cabelos - a combinação perfeita de cuidados para realçar sua beleza.
                                 </p>
                             </div>
@@ -46,7 +46,7 @@ export default function ListProduct() {
                                 <button className="bg-greenEco-100 text-white max-w-[185px] w-full p-3">Ver mais</button>
                             </div>
                         </div>
-                        <SlidesMiniCard />
+                        {/* <SlidesMiniCard /> */}
                     </div>
                     <div className="flex justify-center flex-col">
                         <div className='w-full text-center mt-[40px]'>
