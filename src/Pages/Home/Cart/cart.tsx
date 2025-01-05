88695908b94b080d2b91be4f5b2020a10210c7e8
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2025-01-05 10:34:14 -0300

    style

--- src/Pages/Home/Cart/cart.tsx
+++ src/Pages/Home/Cart/cart.tsx
@@ -16,9 +16,9 @@ export default function Cart() {
         <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
+            className="h-screen"
         >
             {loadingCart ? (
-                // Exibe o componente de carregamento enquanto os dados estão sendo carregados.
                 <Loading styleLoading="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" />
             ) : cart.length > 0 ? (
                 <>
@@ -53,8 +53,14 @@ export default function Cart() {
                     </div>
                 </>
             ) : (
-                // Exibe a mensagem quando o carrinho estiver vazio.
-                <span className="p-[50px] text-2xl font-bold text-zinc-600">Seu carrinho está vazio.</span>
+                <div className="h-full flex justify-center items-center">
+                    <span className="p-[50px] text-3xl font-bold text-zinc-600 flex items-center gap-4">
+                        seu carrinho está vazio
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-[30px] h-[30px]">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+                    </span>
+                </div>
             )}
             <ToastContainer />
         </motion.div>
