61cd14fb97af24108479b57b8085aef94180d39b
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2025-01-05 10:34:01 -0300

    fix: permitindo números decimais ao editar preço do produto

--- src/components/Form-edit-product/form-edit-product.tsx
+++ src/components/Form-edit-product/form-edit-product.tsx
@@ -298,6 +298,9 @@ export default function FormEditProduct(props: PropsForm) {
                                         {...register("preco", {
                                             setValueAs: (value) => value == "" ? undefined : Number(value)
                                         })}
+                                        inputProps={{
+                                            step: "any"
+                                        }}
                                     />
                                     {
                                         errors.preco && errors.preco.message && (
