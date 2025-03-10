c004174d3a0499746bac775ed097bc9590332e96
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2025-03-10 15:08:48 -0300

    refactor:  novo card

--- src/components/Card-Product/card-product.tsx
+++ src/components/Card-Product/card-product.tsx
@@ -3,6 +3,7 @@ import ProdutosDTO from '../../types/produto'
 import Moeda from '../../utils/moeda';
 import useCart from '../../hook/useCart';
 import Loading from '../Loading/loading';
+import BagIcon from '../../svg/bag-icon';
 // import imgProduct from '../../assets/produto-1.webp'
 
 interface PropsProduto {
@@ -14,15 +15,36 @@ export default function CardProduct(props: PropsProduto) {
     const { handleAddCart, loadingAddCart } = useCart();
 
     return (
-        <div className={`text-center`}>
-            <Link to={`/detalhes-produtos/${props.iProduto.id_produto}`}>
-                <img src={props.iProduto.imagem && props.iProduto.imagem[0] && props.iProduto.imagem[0].url} alt="" className='py-5 max-w-[200px] h-[200px] w-full' />
-            </Link>
-            <Link to={`/detalhes-produtos/${props.iProduto.id_produto}`}>{props.iProduto.nome_produto}</Link>
-            <p className={`font-bold ${props.iProduto.oferta ? "text-green-600" : ""}`}>{Moeda.formatar(props.iProduto.preco)}</p>
-            <div className='flex justify-center'>
+        <div className='w-full border border-1 border-zinc-500 mb-5'>
+            <img src={
+                props.iProduto &&
+                props.iProduto.imagem &&
+                props.iProduto.imagem[0] &&
+                props.iProduto.imagem[0].url
+            }
+                className='w-full max-w-[300px] h-[300px]'
+                alt={props.iProduto.nome_produto}
+            />
+            <div className='px-3'>
+                <div>
+                    <span className='font-bold font-jura text-xl'>{props.iProduto.nome_produto}</span>
+                </div>
+                <div>
+                    {
+                        props.iProduto.oferta ? <span className='text-2xl font-bold text-green-500'>{Moeda.formatar(props.iProduto.preco)}</span> :
+                            <span className='text-2xl font-bold'>{Moeda.formatar(props.iProduto.preco)}</span>
+                    }
+                </div>
+            </div>
+            <div className='flex'>
+                <Link to={`/detalhes-produtos/${props.iProduto.id_produto}`} className='w-full'>
+                    <button className='text-[20px] bg-greenEco-200 w-full text-white p-1 font-jura'>Ver produto</button>
+                </Link>
                 {
-                    loadingAddCart ? <Loading /> : <button className="p-2 border border-1 border-black w-full" onClick={() => handleAddCart(props.iProduto.id_produto)}>Adicionar ao carrinho</button>
+                    loadingAddCart ? <Loading /> :
+                        <button onClick={() => handleAddCart(props.iProduto.id_produto)} className='text-[20px] bg-zinc-800 w-[50%] flex justify-center items-center'>
+                            <BagIcon />
+                        </button>
                 }
             </div>
         </div>
