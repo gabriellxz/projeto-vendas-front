13a4ce435b6d4d553b29734b1273ffe80892aa53
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2025-03-12 15:50:27 -0300

    style: mudando título

--- src/components/Catalog/catalog.tsx
+++ src/components/Catalog/catalog.tsx
@@ -1,43 +1,40 @@
 import { useParams } from "react-router-dom";
 import useListProduct from "../../hook/useListProduct"
-import ProdutosDTO from "../../types/produto"
 import CardProduct from "../Card-Product/card-product"
 import Loading from "../Loading/loading"
+import { Divider } from "@mui/material";
 
-interface Props {
-    searchItem?: string;
-}
-
-export default function Catalog(props: Props) {
+export default function Catalog() {
 
     const params = useParams()
     const { product, loading } = useListProduct()
 
-    const filterProduct = product.filter((product: ProdutosDTO) =>
-        product.nome_produto.toLocaleLowerCase().includes(props.searchItem ? props.searchItem.toLowerCase() : "")
-    )
-
-    const filterForRecomended = params.idProduto ?
+    const filteredProduct = params.idProduto ?
         product.filter(p => p.id_produto !== parseInt(params.idProduto ? params.idProduto : "0"))
         :
-        filterProduct
+        product
 
     return (
-        <div className="sm:grid sm:grid-cols-3 sm:gap-7 lg:grid-cols-4 grid grid-cols-2 gap-5 mt-5 pt-10 p-10 card-item">
+        <div className="mt-5">
+            <div className="mb-5">
+                <span className="text-3xl uppercase font-extrabold text-zinc-700 italic tracking-tight">Nossos produtos</span>
+                <Divider />
+            </div>
+            <div className="sm:grid sm:grid-cols-3 sm:gap-7 lg:grid-cols-4 grid grid-cols-1 gap-5 card-item">
 
-            {loading ? (
-                <Loading />
-            ) : (
-                product.length > 0 ? (
-                    (params.idProduto ? filterForRecomended : filterProduct).map(product => (
-                        <CardProduct key={product.id_produto} iProduto={product} />
-                    ))
+                {loading ? (
+                    <Loading />
                 ) : (
-                    <p>Não existem produtos...</p>
+                    filteredProduct.length > 0 ? (
+                        filteredProduct.map(p => (
+                            <CardProduct iProduto={p} key={p.id_produto} />
+                        ))
+                    ) : (
+                        <p>Não existem produtos...</p>
+                    )
                 )
-            )}
-
-
+                }
+            </div>
         </div>
     )
 }
