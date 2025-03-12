4e6055445a6019ed3a1dea2341e6caa98649a037
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2025-03-12 15:53:24 -0300

    fix: concertando renderização da busca de produtos

--- src/Pages/Home/CategoriesId/categoriesId.tsx
+++ src/Pages/Home/CategoriesId/categoriesId.tsx
@@ -2,35 +2,46 @@ import { useParams } from "react-router-dom"
 import useListProduct from "../../../hook/useListProduct"
 import CardProduct from "../../../components/Card-Product/card-product"
 import Footer from "../../../components/Footer/footer"
+import { useSearch } from "../../../context/searchContext"
 
 export default function CategoriesId() {
 
     const params = useParams()
     const { product } = useListProduct()
+    const { searchTerm } = useSearch()
 
     const filteredProducts = product.filter(p => p.categoryId == Number(params.categoriaId))
+
+    const filterCondition = searchTerm ? product : filteredProducts
+    const filteredSearchProduct = filterCondition.filter(p =>
+        p.nome_produto.toLowerCase().includes(searchTerm.toLowerCase())
+    )
 
     return (
         <div>
             <div className="flex justify-center">
                 <div className="bg-whiteEco-100 p-5 max-w-[1300px] w-full">
-                    <span className="text-2xl font-bold uppercase">{params.categoria}</span>
+                    <span className="text-2xl font-bold uppercase">
+                        <span>{searchTerm}</span>
+                    </span>
                 </div>
             </div>
-            <div className="flex justify-center">
-                {
-                    filteredProducts.length < 1 ? (
-                        <div className="h-screen flex items-center">
-                            <span className="text-2xl">Não existem produtos nesta categoria</span>
-                        </div>
-                    ) : (
-                        <div className="sm:grid sm:grid-cols-3 lg:grid-cols-3 grid grid-cols-2 my-5 card-item gap-1">
-                            {filteredProducts.map(product => (
-                                <CardProduct iProduto={product} key={product.id_produto} />
-                            ))}
-                        </div>
-                    )
-                }
+            <div className="flex justify-center h-screen">
+                <div>
+                    {
+                        filteredSearchProduct.length < 1 ? (
+                            <div className="h-screen flex items-center">
+                                <span className="text-2xl text-center">Não existem produtos nesta categoria</span>
+                            </div>
+                        ) : (
+                            <div className="sm:grid sm:grid-cols-3 lg:grid-cols-3 grid grid-cols-2 my-5 card-item gap-1">
+                                {filteredSearchProduct.map(product => (
+                                    <CardProduct iProduto={product} key={product.id_produto} />
+                                ))}
+                            </div>
+                        )
+                    }
+                </div>
             </div>
             <Footer />
         </div>
